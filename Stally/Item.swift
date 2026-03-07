709e2935ac021d9534998ce6cde43c7e990e3290
muhiro_ <muhiro.12@gmail.com>
2026-03-07 21:40:32 +0900

    Initial Commit

---
+++ Stally/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Stally
+//
+//  Created by Hiromu Nakano on 2026/03/07.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
