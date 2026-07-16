c30d8c77b4c091a54a6931d28d73db97358c094e
muhiro_ <muhiro.12@gmail.com>
2026-07-16 09:11:27 +0900

    Allow package plugins in Xcode Cloud

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Stally
+#
+#  Created by Hiromu Nakano on 2026/07/16.
+#
+
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
